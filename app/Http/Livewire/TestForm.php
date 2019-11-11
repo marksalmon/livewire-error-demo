1eb8822370488e97865029399fde1861aad4aae2
Mark Salmon <marksalmon@mark.salmon@mediconesystems.com>
2019-11-11 15:17:06 +0000

    wip

---
+++ app/Http/Livewire/TestForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TestForm extends Component
+{
+    public $plop;
+
+
+    public function render()
+    {
+        return view('livewire.test-form');
+    }
+}
---
+++ resources/views/livewire/test-form.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <form class="mt-10 p-4 bg-blue-200 rounded">
+        <input type="text" wire:model="plop">
+        <span class="block text-3xl text-red-600">{{ $plop }}</span>
+    </form>
+</div>
